2826186a90d1d7070123ca87a7ee094070e224ed
Thomas Capelle <thomas.capelle@steady-sun.com>
2021-11-30 11:58:32 +0100

    blackify

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,16 +2,17 @@ from pathlib import Path
 from tests import TEST_PATH
 from nb_helpers.utils import is_nb, find_nbs
 
+
 def test_is_nb():
     bad_names = [
-        Path('folder/.ipynb_checkpoints'),
-        Path('folder/_file.ipynb'),
-        Path('folder/checkpoint_file.ipynb')
+        Path("folder/.ipynb_checkpoints"),
+        Path("folder/_file.ipynb"),
+        Path("folder/checkpoint_file.ipynb"),
     ]
     assert not all([is_nb(f) for f in bad_names])
+
 
 def test_find_nbs():
     valid_nbs = [f.name for f in find_nbs(TEST_PATH)]
     assert len(valid_nbs) == 2
-    assert ('test_nb.ipynb' in valid_nbs) and ('test_nb2.ipynb' in valid_nbs)
-    
+    assert ("test_nb.ipynb" in valid_nbs) and ("test_nb2.ipynb" in valid_nbs)
