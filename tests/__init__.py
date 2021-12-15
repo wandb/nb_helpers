3931107ab02cfc1f2022c3fb75ea50779af29975
Thomas Capelle <tcapelle@pm.me>
2021-12-15 15:20:56 +0100

    add nbs to tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
 TEST_PATH = Path(__file__).parent
-
-TEST_NB = TEST_PATH/'data/test_nb.ipynb'
+TEST_NB = TEST_PATH / "data/test_nb.ipynb"
+FAIL_NB = TEST_PATH / "data/dummy_folder/fail_nb.ipynb"
+SKIP_NB = TEST_PATH / "data/dummy_folder/test_nb_all_slow.ipynb"
+FLAG_NB = TEST_PATH / 'data/dummy_folder/test_nb_all_slow.ipynb'
