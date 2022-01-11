6539a9469e36832b42d9e7e0cf6bb51edc9d515e
Thomas Capelle <tcapelle@pm.me>
2022-01-11 17:51:58 +0100

    refactor and fix colab

--- nb_helpers/wandb.py
+++ nb_helpers/wandb.py
@@ -3,9 +3,11 @@ import re
 from pathlib import Path
 from fastcore.script import Param, call_parse
 from fastcore.basics import listify
+import nbformat
 
-from nb_helpers.utils import git_local_repo, search_string_in_nb, is_nb, read_nb, find_nbs, Logger
-from nb_helpers.colab import get_colab_url
+
+from nb_helpers.utils import git_local_repo, search_string_in_nb, read_nb, find_nbs, Logger, write_nb
+from nb_helpers.colab import add_colab_badge, get_colab_url, _has_colab_badge
 
 
 WANDB_FEATURES = "Table,sweep,WandbCallback,WandbLogger,Artifact"
@@ -24,7 +26,7 @@ def get_wandb_tracker(nb):
     return ""
 
 
-def _search_code(nb, features=WANDB_FEATURES):
+def search_code(nb, features=WANDB_FEATURES):
     "Search notebook for features"
     present_features = []
     for feat in listify(features.split(",")):
@@ -39,10 +41,11 @@ def summary_nbs(
     wandb_features: Param("wandb features to identify, comma separated", str) = WANDB_FEATURES,
     python_libs: Param("Python lib names to filter, eg: tensorflow. Comma separated", str) = PYTHON_LIBS,
     out_file: Param("Export to csv file", Path) = "summary.csv",
+    fix_colab_cell_idx: Param("Cell idx where the colab badge must go", int) = -1,
 ):
     path = Path(path)
     out_file = (path.parent / out_file).with_suffix(".csv")
-    logger = Logger(columns=["#", "nb name", "tracker", "wandb features", "python libs"], out_file=out_file)
+    logger = Logger(columns=["#", "nb name", "tracker", "wandb features", "python libs", "colab_cell"], out_file=out_file)
 
     files = find_nbs(path)
     assert len(files) > 0, "There is no `ipynb` notebooks in the path you submited"
@@ -55,11 +58,28 @@ def summary_nbs(
         nb = read_nb(nb_path)
         tracker_id = get_wandb_tracker(nb)
         fname = nb_path.relative_to(repo_path)
-        features = _search_code(nb, wandb_features)
-        libs = _search_code(nb, python_libs)
-
-        row = [f"{i+1}", str(fname), tracker_id, ", ".join(features), ", ".join(libs)]
+        features = search_code(nb, wandb_features)
+        libs = search_code(nb, python_libs)
+        colab_cell_idx = _has_colab_badge(nb)
+        row = [f"{i+1}", str(fname), tracker_id, ", ".join(features), ", ".join(libs), str(colab_cell_idx) if colab_cell_idx!=-1 else "" ]
         colab_link = get_colab_url(nb_path)
         logger.writerow(row, colab_link)
 
     logger.finish()
+
+@call_parse
+def fix_nbs(
+    path: Param("A path to nb files", str) = ".",
+    colab_cell_idx: Param("Cell idx where the colab badge must go", int) = -1,
+    branch: Param("The branch", str) = "main",
+):
+
+    path = Path(path)
+    files = find_nbs(path)
+    assert len(files) > 0, "There is no `ipynb` notebooks in the path you submited"
+
+    for i, nb_path in enumerate(files):
+        if colab_cell_idx != -1:
+            print(f"Add badge to {nb_path}")
+            nb = add_colab_badge(nb_path, branch=branch, idx=colab_cell_idx)
+            write_nb(nb, nb_path)
