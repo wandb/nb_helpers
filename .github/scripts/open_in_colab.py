2441c5be6de2fe699dc3f42070cb095b8635c4bc
Thomas Capelle <tcapelle@pm.me>
2022-02-03 18:41:54 +0100

    create PR table from colabs

--- .github/scripts/open_in_colab.py
+++ .github/scripts/open_in_colab.py
@@ -1,13 +1,28 @@
 from fastcore.all import *
 from ghapi.all import *
+from nb_helpers.colab import get_colab_url
+from nb_helpers.utils import git_current_branch
 
-def reply_thanks():
+def create_comment():
     api = GhApi(owner='wandb', repo='nb_helpers', token=github_token())
     payload = context_github.event
     if 'workflow' in payload: issue = 1
     else:
         if payload.action != 'opened': return
         issue = payload.number
-    api.issues.create_comment(issue_number=issue, body='Thank you for your *valuable* contribution')
+    
+    pr_files = [f.filename for f in api.pulls.list_files(issue)]
+    nb_files = [f for f in pr_files if ".ipynb" in f]
 
-reply_thanks()
+    def get_colab_md(fname):
+        _fname = Path(fname).name
+        url = get_colab_url(fname, git_current_branch(fname))
+        return f"- [{_fname}]({url})\n"
+
+    colab_links = tuple(get_colab_md(f) for f in nb_files)
+    body = ("The following colabs where changed in this PR:\n",)+colab_links
+    body = "".join(body)
+
+    api.issues.create_comment(issue_number=issue, body=body)
+
+create_comment()
