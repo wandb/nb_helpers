3fe77dbe2ec9522f1f4658375b118336420c53ae
Thomas Capelle <tcapelle@pm.me>
2022-02-04 10:19:56 +0100

    black

--- .github/scripts/open_in_colab.py
+++ .github/scripts/open_in_colab.py
@@ -3,19 +3,22 @@ from ghapi.all import *
 from nb_helpers.colab import get_colab_url
 from nb_helpers.utils import git_current_branch, is_nb
 
+
 def create_comment():
     "On PR post a comment with links to open in colab for each changed nb"
 
-    api = GhApi(owner='wandb', repo='nb_helpers', token=github_token())
+    api = GhApi(owner="wandb", repo="nb_helpers", token=github_token())
     payload = context_github.event
-    if 'workflow' in payload: issue = 1
+    if "workflow" in payload:
+        issue = 1
     else:
-        if payload.action != 'opened': return
+        if payload.action != "opened":
+            return
         issue = payload.number
-    
+
     pr_files = [Path(f.filename) for f in api.pulls.list_files(issue)]
-    
-    #filter nbs
+
+    # filter nbs
     nb_files = [f for f in pr_files if is_nb(f)]
 
     def _get_colab_url2md(fname: Path) -> str:
@@ -29,10 +32,11 @@ def create_comment():
         colab_links = tuple(_get_colab_url2md(f) for f in nb_files)
         body = tuplify(title) + colab_links
         return "".join(body)
-        
-    if len(nb_files)>0:
+
+    if len(nb_files) > 0:
         body = _create_comment_body(nb_files)
         print(f">> Creating comment on PR #{issue}")
         api.issues.create_comment(issue_number=issue, body=body)
-    
+
+
 create_comment()
