d23818317b1a579179af78a61b1b1fbdc995603f
Thomas Capelle <tcapelle@pm.me>
2022-11-04 23:11:50 +0100

    context

--- nb_helpers/actions.py
+++ nb_helpers/actions.py
@@ -73,7 +73,7 @@ def upload_at(owner="wandb", repo="nb_helpers", token=None):
     
 def download(owner="wandb", repo="nb_helpers", token=None):
     api, payload = get_api(owner, repo, token)
-    print(api.actions.list_workflow_run_artifacts(github_context.run_id))
+    print(api.actions.list_workflow_run_artifacts(context_github.run_id))
 
 # %% ../nbs/03_actions.ipynb 17
 def update_comment(issue, body, comment_id, owner="wandb", repo="nb_helpers", token=None):
