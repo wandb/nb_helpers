3ac8ff697cad3b9841b6f7cb00508060aed3a095
Thomas Capelle <tcapelle@pm.me>
2023-03-22 18:01:26 +0100

    full path to header

--- .github/scripts/upload_notebooks.py
+++ .github/scripts/upload_notebooks.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from nb_helpers.actions import upload_modified_nbs
 
-upload_modified_nbs("tcapelle", "nb_helpers", pr_message="pr_message.md")
+upload_modified_nbs("tcapelle", "nb_helpers", pr_message=Path.cwd()/"pr_message.md")
