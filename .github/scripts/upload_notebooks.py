e170602f1ba14a6a43b816b8fe7b32360dfc8a03
Thomas Capelle <tcapelle@pm.me>
2023-03-22 18:09:33 +0100

    use __file__ current dir

--- .github/scripts/upload_notebooks.py
+++ .github/scripts/upload_notebooks.py
@@ -2,4 +2,6 @@ from pathlib import Path
 
 from nb_helpers.actions import upload_modified_nbs
 
-upload_modified_nbs("tcapelle", "nb_helpers", pr_message=Path.cwd()/"pr_message.md")
+scripts_folder = Path(__file__).resolve().parent
+
+upload_modified_nbs("tcapelle", "nb_helpers", pr_message=scripts_folder/"pr_message.md")
